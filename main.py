f0bc5d42d8b8b84d0ec3d31c6ffb945d59480a69
Ilya Antonov <ilya.anton0v@vk.com>
2023-03-12 23:20:45 +0300

    add clear array function

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import sys
 
 class Tree(QtWidgets.QMainWindow):
-    queueCreated = False
+    treeArray = []
 
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
@@ -13,19 +13,29 @@ class Tree(QtWidgets.QMainWindow):
 
         self.ui.quit.clicked.connect(self.quit)
         self.ui.createQueue.clicked.connect(self.createArray)
+        self.ui.clearQueue.clicked.connect(self.clearTable)
 
     def quit(self):
         self.ui.quit.clicked.connect(QtWidgets.QApplication.instance().quit)
     
     def createArray(self):
-        if self.queueCreated:
+        if len(self.treeArray) != 0:
             QtWidgets.QMessageBox.about(self, "Ошибка!", "Очередь уже создана")
             return
         
-        array = [random.randint(10, 99) for number in range(15)]
-        self.queueCreated = True
-        for i in range(len(array)):
-            self.ui.arrayView.setItem(0, i, QtWidgets.QTableWidgetItem(str(array[i])))
+        self.treeArray = [random.randint(10, 99) for number in range(15)]
+        for i in range(len(self.treeArray)):
+            self.ui.arrayView.setItem(0, i, QtWidgets.QTableWidgetItem(str(self.treeArray[i])))
+
+    def clearTable(self):
+        if len(self.treeArray) == 0:
+            QtWidgets.QMessageBox.about(self, "Ошибка!", "Очередь уже очищена")
+            return
+        
+        for i in range(len(self.treeArray)):
+            self.ui.arrayView.setItem(0, i, QtWidgets.QTableWidgetItem(str("")))
+        
+        self.treeArray.clear()
 
 
 if __name__ == "__main__":
