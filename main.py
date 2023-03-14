6787b5b8a6d1d7c7c91b7953f45a09e9597b55e6
Ilya Antonov <ilya.anton0v@vk.com>
2023-03-14 08:35:59 +0300

    lab17

--- main.py
+++ main.py
@@ -31,21 +31,21 @@ class Tree(QtWidgets.QMainWindow):
         for i in range(len(self.treeArray)):
             self.ui.arrayView.setItem(0, i, QtWidgets.QTableWidgetItem(str(self.treeArray[i])))
 
-        position = int(len(self.treeArray) / 2)
-        i = 0
-        rows = 0
-        maxEleminRow = 1
-        while rows < self.ui.treeView.rowCount():
-            j = 0
-            firstElemInRow = position
-            while maxEleminRow > j and i < len(self.treeArray):
-                self.ui.treeView.setItem(rows, firstElemInRow, QtWidgets.QTableWidgetItem(str(self.treeArray[i])))
-                i += 1
-                firstElemInRow += position * 2 + 2
-                j += 1
-            position //= 2
-            maxEleminRow *= 2
-            rows += 1
+        self.ui.treeView.setItem(0, 7, QtWidgets.QTableWidgetItem(str(self.treeArray[0])))
+        self.ui.treeView.setItem(1, 3, QtWidgets.QTableWidgetItem(str(self.treeArray[1])))
+        self.ui.treeView.setItem(1, 11, QtWidgets.QTableWidgetItem(str(self.treeArray[2])))
+        self.ui.treeView.setItem(2, 1, QtWidgets.QTableWidgetItem(str(self.treeArray[3])))
+        self.ui.treeView.setItem(2, 5, QtWidgets.QTableWidgetItem(str(self.treeArray[4])))
+        self.ui.treeView.setItem(2, 9, QtWidgets.QTableWidgetItem(str(self.treeArray[5])))
+        self.ui.treeView.setItem(2, 13, QtWidgets.QTableWidgetItem(str(self.treeArray[6])))
+        self.ui.treeView.setItem(3, 0, QtWidgets.QTableWidgetItem(str(self.treeArray[7])))
+        self.ui.treeView.setItem(3, 2, QtWidgets.QTableWidgetItem(str(self.treeArray[8])))
+        self.ui.treeView.setItem(3, 4, QtWidgets.QTableWidgetItem(str(self.treeArray[9])))
+        self.ui.treeView.setItem(3, 6, QtWidgets.QTableWidgetItem(str(self.treeArray[10])))
+        self.ui.treeView.setItem(3, 8, QtWidgets.QTableWidgetItem(str(self.treeArray[11])))
+        self.ui.treeView.setItem(3, 10, QtWidgets.QTableWidgetItem(str(self.treeArray[12])))
+        self.ui.treeView.setItem(3, 12, QtWidgets.QTableWidgetItem(str(self.treeArray[13])))
+        self.ui.treeView.setItem(3, 14, QtWidgets.QTableWidgetItem(str(self.treeArray[14])))
 
         
     def clearTable(self):
