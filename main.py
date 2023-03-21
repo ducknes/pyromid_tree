9cb797dccf486e9ac40c4220deb1e6101b955522
Ilya Antonov <ilya.anton0v@vk.com>
2023-03-21 08:49:49 +0300

    lab18 final

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from Form import *
 import random
 import sys
+import numpy as np
+
 
 class Tree(QtWidgets.QMainWindow):
-    treeArray = []
+    treeArray = np.empty(16, dtype=np.int64)
+    current_size = 0
 
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
@@ -19,49 +22,50 @@ class Tree(QtWidgets.QMainWindow):
         self.ui.quit.clicked.connect(QtWidgets.QApplication.instance().quit)
     
     def createArray(self):
-        if len(self.treeArray) >= 1:
-            QtWidgets.QMessageBox.about(self, "Ошибка!", "Очередь уже создана")
+        if self.current_size > 0:
+            QtWidgets.QMessageBox.about(self, "Ошибка!", "Очередь заполнена")
             return
         
-        self.treeArray = [random.randint(10, 99) for number in range(15)]
+        for i in range(1, 16):
+            self.treeArray[i] = random.randint(10, 99)
+            self.fixUp(i)
+        self.current_size = 15
         self.printArray()
         
-
     def printArray(self):
-        for i in range(len(self.treeArray)):
-            self.ui.arrayView.setItem(0, i, QtWidgets.QTableWidgetItem(str(self.treeArray[i])))
+        for i in range(len(self.treeArray) - 1):
+            self.ui.arrayView.setItem(0, i, QtWidgets.QTableWidgetItem(str(self.treeArray[i + 1])))
 
-        self.ui.treeView.setItem(0, 7, QtWidgets.QTableWidgetItem(str(self.treeArray[0])))
-        self.ui.treeView.setItem(1, 3, QtWidgets.QTableWidgetItem(str(self.treeArray[1])))
-        self.ui.treeView.setItem(1, 11, QtWidgets.QTableWidgetItem(str(self.treeArray[2])))
-        self.ui.treeView.setItem(2, 1, QtWidgets.QTableWidgetItem(str(self.treeArray[3])))
-        self.ui.treeView.setItem(2, 5, QtWidgets.QTableWidgetItem(str(self.treeArray[4])))
-        self.ui.treeView.setItem(2, 9, QtWidgets.QTableWidgetItem(str(self.treeArray[5])))
-        self.ui.treeView.setItem(2, 13, QtWidgets.QTableWidgetItem(str(self.treeArray[6])))
-        self.ui.treeView.setItem(3, 0, QtWidgets.QTableWidgetItem(str(self.treeArray[7])))
-        self.ui.treeView.setItem(3, 2, QtWidgets.QTableWidgetItem(str(self.treeArray[8])))
-        self.ui.treeView.setItem(3, 4, QtWidgets.QTableWidgetItem(str(self.treeArray[9])))
-        self.ui.treeView.setItem(3, 6, QtWidgets.QTableWidgetItem(str(self.treeArray[10])))
-        self.ui.treeView.setItem(3, 8, QtWidgets.QTableWidgetItem(str(self.treeArray[11])))
-        self.ui.treeView.setItem(3, 10, QtWidgets.QTableWidgetItem(str(self.treeArray[12])))
-        self.ui.treeView.setItem(3, 12, QtWidgets.QTableWidgetItem(str(self.treeArray[13])))
-        self.ui.treeView.setItem(3, 14, QtWidgets.QTableWidgetItem(str(self.treeArray[14])))
+        self.ui.treeView.setItem(0, 7, QtWidgets.QTableWidgetItem(str(self.treeArray[1])))
+        self.ui.treeView.setItem(1, 3, QtWidgets.QTableWidgetItem(str(self.treeArray[2])))
+        self.ui.treeView.setItem(1, 11, QtWidgets.QTableWidgetItem(str(self.treeArray[3])))
+        self.ui.treeView.setItem(2, 1, QtWidgets.QTableWidgetItem(str(self.treeArray[4])))
+        self.ui.treeView.setItem(2, 5, QtWidgets.QTableWidgetItem(str(self.treeArray[5])))
+        self.ui.treeView.setItem(2, 9, QtWidgets.QTableWidgetItem(str(self.treeArray[6])))
+        self.ui.treeView.setItem(2, 13, QtWidgets.QTableWidgetItem(str(self.treeArray[7])))
+        self.ui.treeView.setItem(3, 0, QtWidgets.QTableWidgetItem(str(self.treeArray[8])))
+        self.ui.treeView.setItem(3, 2, QtWidgets.QTableWidgetItem(str(self.treeArray[9])))
+        self.ui.treeView.setItem(3, 4, QtWidgets.QTableWidgetItem(str(self.treeArray[10])))
+        self.ui.treeView.setItem(3, 6, QtWidgets.QTableWidgetItem(str(self.treeArray[11])))
+        self.ui.treeView.setItem(3, 8, QtWidgets.QTableWidgetItem(str(self.treeArray[12])))
+        self.ui.treeView.setItem(3, 10, QtWidgets.QTableWidgetItem(str(self.treeArray[13])))
+        self.ui.treeView.setItem(3, 12, QtWidgets.QTableWidgetItem(str(self.treeArray[14])))
+        self.ui.treeView.setItem(3, 14, QtWidgets.QTableWidgetItem(str(self.treeArray[15])))
 
-        
     def clearTable(self):
-        if len(self.treeArray) == 0:
-            QtWidgets.QMessageBox.about(self, "Ошибка!", "Очередь уже очищена")
+        if self.current_size == 0:
+            QtWidgets.QMessageBox.about(self, "Ошибка!", "Очередь пуста")
             return
 
+        self.current_size = 0
         self.ui.arrayView.clear()
         self.ui.treeView.clear()
         
-        self.treeArray.clear()
+        np.delete(self.treeArray, 0)
 
-    def fixUp(self):
-        k = len(self.treeArray)
-        while k > 1 and self.treeArray[k / 2] < self.treeArray[k]:
-            self.treeArray[k], self.treeArray[k / 2] = self.treeArray[k / 2], self.treeArray[k]
+    def fixUp(self, k):
+        while k > 1 and self.treeArray[int(k / 2)] < self.treeArray[k]:
+            self.treeArray[k], self.treeArray[int(k / 2)] = self.treeArray[int(k / 2)], self.treeArray[k]
             k //= 2
         
 
